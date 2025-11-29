cfa9be7cd391547e9fef1d7e4c3b457f67d527b4
Emanuely Tauany <etsf1@aluno.ifal.edu.br>
2025-11-29 19:09:47 -0300

    Correção na geração de certificados

--- modules/certificados/certificados.py
+++ modules/certificados/certificados.py
@@ -1,11 +1,13 @@
 from prisma import Prisma
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import A4, landscape
 from reportlab.lib.units import cm
+from reportlab.lib.utils import ImageReader
+import os
  
 db = Prisma()
 
-async def gerar_certificado(participante_id, evento_id, data):
+async def gerar_certificado(participante_id:int, evento_id: int, data: str):
      
    await db.connect()
     
@@ -19,25 +21,55 @@ async def gerar_certificado(participante_id, evento_id, data):
     
    await db.disconnect()
 
+   if not participante:
+     raise ValueError("Participante não encontrado.")
+   if not evento:
+     raise ValueError("Evento não encontrado.")
+
    nome = participante.nome
    nome_evento = evento.nome
-   carga_horaria =participante.carga_horaria
-
-   caminho_arquivo = f"@/certificado_{nome.replace(' ', '_')}.pdf"
+   
+   
+   pasta = "certificados"
+   os.makedirs(pasta, exist_ok=True)
+   caminho_arquivo = os.path.join(pasta, f"certificado_{nome.replace(' ', '_')}.pdf")
     
-   pdf = canvas.Canvas(caminho_arquivo, pagesize=A4)
-   largura, altura = A4
+   pdf = canvas.Canvas(caminho_arquivo, pagesize=landscape(A4))
+   largura, altura = landscape(A4)
+
+   caminho_logo = "certificados/logo_ifal.png"
+
+   if os.path.exists(caminho_logo):
+     largura_logo = 7*cm
+     altura_logo =  7*cm
+     
+     x_logo = (largura - largura_logo) /2
+     y_logo = altura - 10*cm
+
+     pdf.drawImage(caminho_logo, x_logo, y_logo,
+                   width=largura_logo, height=altura_logo,
+                   preserveAspectRatio=True, mask='auto')
 
    pdf.setFont("Helvetica-Bold", 28)
-   pdf.drawCentredString(largura/2, altura - 7*cm, "Certificado")
+   pdf.drawCentredString(largura/2, altura - 10.5*cm, "Certificado")
    pdf.setFont("Helvetica", 14)
+   linha1 = (
+     f"Certificamos que {nome} participou do evento " 
+     f"'{nome_evento}', realizado em {data}"
+   )
+
+   linha2 = ("com total de quarenta (40) horas de atividade.")
    pdf.drawCentredString(
-       largura/2, alutura - 10*cm, 
-       f"Certificamos que {nome} participor do evento '{nome_evento} ({carga_horaria}h)"
-    )
+       largura/2, altura - 12*cm, linha1
+     )
+
+   pdf.drawCentredString(
+       largura/2, altura -13.2*cm, linha2
+     )
+
    pdf.save()
    print("Certificado gerado:", caminho_arquivo)
     
    with open("certificados.txt","a", encoding="utf-8") as arquivo:
-        arquivo.write(f"{nome} - {carga_horaria}h\n")
+        arquivo.write(f"{nome} - 40h\n")
 
